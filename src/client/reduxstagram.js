cb4e415bcb32d04ee46eb16462038d7733cfa098
Balint Soos <soosbalint95@gmail.com>
2016-06-07 15:00:58 +0200

    react boilerplate

---
+++ src/client/reduxstagram.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// import css
+// import css from './styles/style.styl'
+
+render(<p>Hello</p>, document.getElementById('root'))
--- src/webpack.config.dev.js
+++ src/webpack.config.dev.js
@@ -22,12 +22,12 @@ module.exports = {
       {
         test: /\.js$/,
         loaders: ['babel'],
-        include: path.join(__dirname, 'src', 'client')
+        include: path.join(__dirname, 'client')
       },
       // CSS
       {
         test: /\.styl$/,
-        include: path.join(__dirname, 'src', 'client'),
+        include: path.join(__dirname, 'client'),
         loader: 'style-loader!css-loader!stylus-loader'
       }
     ]
