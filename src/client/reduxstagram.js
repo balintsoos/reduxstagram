0074513b47c5998cfb21701638a64b604c90151c
Balint Soos <soosbalint95@gmail.com>
2016-06-07 15:58:00 +0200

    Main component

--- src/client/reduxstagram.js
+++ src/client/reduxstagram.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { render } from 'react-dom'
 
 // import css
-// import css from './styles/style.styl'
+import css from './styles/style.styl'
 
-render(<p>Hello</p>, document.getElementById('root'))
+// import components
+import Main from './components/Main'
+
+render(<Main />, document.getElementById('root'))
