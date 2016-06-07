7545675e9378fc42a1f262c7569ec9431ff8fa38
Balint Soos <soosbalint95@gmail.com>
2016-06-07 17:29:01 +0200

    client side routing with react-router

--- src/client/reduxstagram.js
+++ src/client/reduxstagram.js
@@ -7,5 +7,19 @@ import css from './styles/style.styl'
 
 // import components
 import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
-render(<Main />, document.getElementById('root'))
+// import react router
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path='view/:id' component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'))
