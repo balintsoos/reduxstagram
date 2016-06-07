cefe9b8cc5f7357055357e258d54fef5dd432276
Balint Soos <soosbalint95@gmail.com>
2016-06-07 11:11:36 +0200

    development server added

---
+++ src/devServer.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+const webpack = require('webpack')
+const config = require('./webpack.config.dev')
+const devMiddleware = require('webpack-dev-middleware')
+const hotMiddleware = require('webpack-hot-middleware')
+
+const app = express()
+const compiler = webpack(config)
+
+app.use(devMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+
+app.use(hotMiddleware(compiler))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(3000, 'localhost', (err) => {
+  if (err) {
+    return console.error(err)
+  }
+
+  console.log('Server listening @ http://localhost:3000')
+})
