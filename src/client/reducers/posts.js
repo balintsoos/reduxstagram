0539c3b59f2b62c6b729c9ab88abb7f95eea5f48
Balint Soos <soosbalint95@gmail.com>
2016-06-07 20:35:30 +0200

    posts reducer added

--- src/client/reducers/posts.js
+++ src/client/reducers/posts.js
@@ -1,3 +1,13 @@
 export default function posts(state = [], action) {
-  return state
+  if (action.type !== 'INCREMENT_LIKES') {
+    return state
+  }
+
+  const i = action.index
+
+  return [
+    ...state.slice(0, i),
+    { ...state[i], likes: state[i].likes + 1 },
+    ...state.slice(i + 1)
+  ]
 }
