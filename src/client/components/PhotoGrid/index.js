66aade9b449b381af238e42f22a59700ec6f14c3
Balint Soos <soosbalint95@gmail.com>
2016-06-07 17:28:22 +0200

    PhotoGrid and Single component

---
+++ src/client/components/PhotoGrid/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+      Grid
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid
---
+++ src/client/components/Single/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+      Single
+      </div>
+    )
+  }
+})
+
+export default Single
