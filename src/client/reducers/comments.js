b26a35c3f9497b027a6fca00265e68fffc4b3a1d
Balint Soos <soosbalint95@gmail.com>
2016-06-07 23:11:53 +0200

    comments reducer added

--- src/client/reducers/comments.js
+++ src/client/reducers/comments.js
@@ -1,3 +1,28 @@
+function postComments(state = [], action) {
+  if (action.type === 'ADD_COMMENT') {
+    return [ ...state, {
+      user: action.author,
+      text: action.comment
+    }]
+  }
+
+  if (action.type === 'REMOVE_COMMENT') {
+    return [
+      ...state.slice(0, action.index),
+      ...state.slice(action.index + 1)
+    ]
+  }
+
+  return state
+}
+
 export default function comments(state = [], action) {
+  if (action.type !== undefined) {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+
   return state
 }
